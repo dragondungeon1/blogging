41e13eeefdc22097bc3df9f855fb549945c4c869
dragondungeon1 <lennartderidder@gmail.com>
2021-08-20 17:10:45 +0200

    admin functionalities changed

--- src/Controller/Admin/MicroPostCrudController.php
+++ src/Controller/Admin/MicroPostCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\MicroPost;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class MicroPostCrudController extends AbstractCrudController
 {
@@ -12,14 +15,13 @@ class MicroPostCrudController extends AbstractCrudController
         return MicroPost::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('subject'),
+            TextEditorField::new('text'),
         ];
     }
-    */
+
 }
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +17,26 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+    public function encrypt(UserPasswordEncoderInterface $encoder)
+    {
+        // whatever *your* User object is
+        $user = new User();
+        $plainPassword = '';
+        $encoded = $encoder->encodePassword($user, $plainPassword);
+
+        $user->setPassword($encoded);
+    }
+
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+//            IdField::new('id'),
+            TextField::new('name'),
+            TextField::new('email'),
+            TextField::new('password'),
+            BooleanField::new('isVerified'),
+//            TextEditorField::new('description'),
         ];
     }
-    */
 }
